da2dc38b637263977040aea7011d1e250773d218
wlln14 <williagama63@gmail.com>
2026-02-18 16:27:26 -0300

    inicial

---
+++ src/test/java/com/senai/conta_bancaria/ContaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.conta_bancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
